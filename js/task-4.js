4b271c6746101389d9a367da54ecf3d3f58cd07f
Anatolii Sokolovskyi <sans357@gmail.com>
2025-11-24 18:15:13 +0200

    refactor: clean up task files for better organization

--- js/task-4.js
+++ js/task-4.js
@@ -1,6 +1,6 @@
-const getTotalBalanceByGender = (users, gender) =>
-  users.filter(user => user.gender ===gender)
-.reduce((total, user) => total + user.balance, 0 );
+const getTotalBalanceByGender = (users,gender) => users
+  .filter(user => user.gender === gender)
+  .reduce((total, user) => total + user.balance, 0);
 
 const clients = [
 	{
